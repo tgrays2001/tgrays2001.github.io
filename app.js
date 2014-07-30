9cd2b9e04a08d8e0507a6a106c5efae7ec92011b
tgrays <tgrays2001@gmail.com>
2014-07-29 20:04:16 -0600

    corrected menu open and close

--- app.js
+++ app.js
@@ -8,5 +8,15 @@ var main = function () {
         left: '285px'
     }, 200);
 });
+	
+	  $('.icon-close').click(function(){
+    $('.menu').animate({
+        left: '-285px'
+    }, 200);
+    
+    $('body').animate({
+        left: '0px'
+    }, 200);
+});
 };
 $(document).ready(main);
